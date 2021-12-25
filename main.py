0bb125e08a2239a98da9bc73fb3e6a19dc2bc63d
krisdhinal <krisdhinal@gmail.com>
2021-12-25 12:52:51 +0700

    implementation response model on our api

--- main.py
+++ main.py
@@ -1,13 +1,72 @@
-from fastapi import FastAPI
+# for make object into list or array
+from typing import List
+# fast api liblary for better microservices
+from fastapi import FastAPI, Depends, HTTPException, status
+# model schema from schemas file
 import schemas
+# model and table
 import models
-from database import engine
+# seeting file for connecting table to sqlite
+from database import SessionLocal, engine
+# liblary for query sql leanguage
+from sqlalchemy.orm import Session, session
 
 app = FastAPI()
 
 models.Base.metadata.create_all(engine)
 
 
-@app.post('/blog')
-def create(request: schemas.Blog):
-    return request
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@app.post('/blog', status_code=status.HTTP_201_CREATED)
+def create(request: schemas.Blog, db: Session = Depends(get_db)):
+    new_blog = models.Blog(title=request.title, body=request.body)
+    db.add(new_blog)
+    db.commit()
+    db.refresh(new_blog)
+    return new_blog
+
+
+@app.get('/blog', response_model=List[schemas.LihatBlog])
+def get_all_blog(db: Session = Depends(get_db)):
+    blogs = db.query(models.Blog).all()
+    return blogs
+
+
+@app.get('/blog/{id}', status_code=200, response_model=schemas.LihatBlog)
+def show(id, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id).first()
+    if not blog:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"Blog with id {id} not found")
+        # response.status_code = status.HTTP_404_NOT_FOUND
+        # return {'detail': f'Blog with id {id} not found'}
+    return blog
+
+
+@app.put('/blog/{id}', status_code=status.HTTP_202_ACCEPTED)
+def update_blog(id, request: schemas.Blog, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"Blog with id {id} not found")
+    blog.update(request.dict())
+    db.commit()
+    return {f"the blog with id {id} successfully updated"}
+
+
+@app.delete('/blog/{id}', status_code=status.HTTP_204_NO_CONTENT)
+def delete_blog(id, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"Blog with id {id} not found")
+    blog.delete(synchronize_session=False)
+    db.commit()
+    return {f"the blog with id {id} successfully deleted"}
--- schemas.py
+++ schemas.py
@@ -4,3 +4,12 @@ from pydantic import BaseModel
 class Blog(BaseModel):
     title: str
     body: str
+
+
+class LihatBlog(BaseModel):
+    # untuk menghendel error ( value is not a valid dict (type=type_error.dict))
+    title: str
+    body: str
+
+    class Config():
+        orm_mode = True
